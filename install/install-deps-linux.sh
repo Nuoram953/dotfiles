b32ecb23d37e31192ddce508b62abeb75e493b88
Nuoram953 <69933625+Nuoram953@users.noreply.github.com>
2023-06-28 21:26:09 -0400

    Reverted changes

--- install/install-deps-linux.sh
+++
@@ -1,8 +0,0 @@
-apt-get install the_silver_searcher
-apt-get install tree
-apt-get install youtube-dl
-apt-get install jsonpp
-apt-get install nvm
-apt-get install exa
-apt-get install neovim
-apt-get install python3-neovim
