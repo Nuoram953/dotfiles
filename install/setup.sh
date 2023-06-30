e80bfb282413e36e2e520e3bb3c89d62a1271dc4
nuoram <nuoram>
2023-06-30 12:25:09 -0400

    Updated setup script

--- install/setup.sh
+++ install/setup.sh
@@ -8,5 +8,6 @@ sudo apt upgrade -yu
 
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone --depth=1 ttps://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10kh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
