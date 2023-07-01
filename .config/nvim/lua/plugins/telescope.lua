8c53c7288495c893f2259ffbadb206ad376da8bd
nuoram <nuoram>
2023-07-01 01:24:44 -0400

    Formatted some lua files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,12 +1,16 @@
 return { -- Telescope
--- Find, Filter, Preview, Pick. All lua, all the time.
-{
-    "nvim-telescope/telescope.nvim",
-    dependencies = {"nvim-lua/plenary.nvim", {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make"
-    }},
-    config = function(_)
-        require("telescope").setup()
-    end
-}}
+	-- Find, Filter, Preview, Pick. All lua, all the time.
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+			},
+		},
+		config = function(_)
+			require("telescope").setup()
+		end,
+	},
+}
