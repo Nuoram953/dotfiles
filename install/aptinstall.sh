5692d7ae6e4404223dfcee4fb48f6a927d99cfc3
Nuoram953 <69933625+Nuoram953@users.noreply.github.com>
2023-07-02 00:07:12 -0400

    omz no input from user

--- install/aptinstall.sh
+++ install/aptinstall.sh
@@ -38,4 +38,4 @@ install npm
 install expect
 install sed
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed '/\s*env\s\s*zsh\s*/d')" \
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
