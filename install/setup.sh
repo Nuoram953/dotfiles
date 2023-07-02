7242b5013f24b96227a7ab0dddf8e267deecfbcb
Nuoram953 <69933625+Nuoram953@users.noreply.github.com>
2023-07-01 23:41:36 -0400

    Fixed typo

--- install/setup.sh
+++ install/setup.sh
@@ -26,7 +26,7 @@ if [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
     echo "Directory already exists. Skipping cloning."
 else
     echo "Directory does not exists. Cloning repo..."
-    git clone --depth=1 ttps://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 fi
 
 if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
