03184bb09a64cea5245f0a6c217301750f7c1f79
nuoram <antoinemaroun@gmail.com>
2023-07-02 11:58:10 -0400

    Updated telescope showing hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,10 +3,17 @@ return { -- Telescope
 	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = {
-			"nvim-lua/plenary.nvim"
+			"nvim-lua/plenary.nvim",
 		},
 		config = function(_)
-			require("telescope").setup()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+						find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "-L" },
+					},
+				},
+			})
 		end,
 	},
 }
