b1ff1db90e96e757659c0ae4a9bdd4d9b04916ca
nuoram <antoinemaroun@gmail.com>
2023-07-02 00:52:16 -0400

    Moved plugins into a separate file

---
+++ install/plugins.sh
@@ -0,0 +1,32 @@
+#!/bin/bas
+
+if [ -d ~/.tmux/plugins/tpm ]; then
+    echo "Directory already exists. Skipping cloning."
+else
+    echo "Directory does not exists. Cloning repo..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+    echo "Directory already exists. Skipping cloning."
+else
+    echo "Directory does not exists. Cloning repo..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+
+if [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+    echo "Directory already exists. Skipping cloning."
+else
+    echo "Directory does not exists. Cloning repo..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+    echo "Directory already exists. Skipping cloning."
+else
+    echo "Directory does not exists. Cloning repo..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+
--- install/setup.sh
+++ install/setup.sh
@@ -6,35 +6,7 @@
 # Get all pgrades
 sudo apt upgrade -y
 
-
-if [ -d ~/.tmux/plugins/tpm ]; then
-    echo "Directory already exists. Skipping cloning."
-else
-    echo "Directory does not exists. Cloning repo..."
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
-
-if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
-    echo "Directory already exists. Skipping cloning."
-else
-    echo "Directory does not exists. Cloning repo..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
-
-
-if [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then
-    echo "Directory already exists. Skipping cloning."
-else
-    echo "Directory does not exists. Cloning repo..."
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-fi
-
-if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
-    echo "Directory already exists. Skipping cloning."
-else
-    echo "Directory does not exists. Cloning repo..."
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-fi
+./plugins.sh
 
 #Oh My Zsh
 chsh -s $(which zsh)
