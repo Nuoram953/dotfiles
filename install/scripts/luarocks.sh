7f177ba84c2e75b2ab3ada5ad3b598089e789cf2
nuoram <nuoram>
2023-07-01 21:14:44 -0400

    Created new folder for scripts

---
+++ install/scripts/luarocks.sh
@@ -0,0 +1,13 @@
+#!/bin/bas
+
+curl -R -O http://www.lua.org/ftp/lua-5.3.5.tar.gz
+tar -zxf lua-5.3.5.tar.gz
+cd lua-5.3.5
+make linux test
+sudo make install
+wget https://luarocks.org/releases/luarocks-3.8.0.tar.gz
+tar zxpf luarocks-3.8.0.tar.gz
+cd luarocks-3.8.0
+./configure --with-lua-include=/usr/local/include
+make
+sudo make install
