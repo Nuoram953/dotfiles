cc8eb9e914676b61347c7d1fb6447bfd5e7ad258
Nuoram <antoinemaroun@gmail.com>
2023-11-08 18:48:29 -0500

    updated libraries + ui

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,7 +10,7 @@ vim.keymap.set("t", "ne", "<C-\\><C-n>", {noremap=true})
 -- Telescope
 -- <leader> is a space now
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc="Find files"})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
@@ -21,6 +21,7 @@ vim.keymap.set("n", ";", "<cmd>lua require('telescope.builtin').resume(require('
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {}) -- open/close
 vim.keymap.set("n", "<leader>nr", ":NvimTreeRefresh<CR>", {}) -- refresh
 vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<CR>", {}) -- search file
+vim.keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>", {}) -- search file
 
 -- Symbole-outline
 vim.keymap.set("n", "<leader>so", ":SymbolsOutline<CR>", {})
