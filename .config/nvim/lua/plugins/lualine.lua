92ba164de29fc02756eee71b391b851e36bfcf15
Nuoram <antoinemaroun@gmail.com>
2023-11-08 19:20:56 -0500

    updated lualine for macros

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,9 @@
 return {
     'nvim-lualine/lualine.nvim',
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "meuter/lualine-so-fancy.nvim"
+    },
     config = function(_)
 
 require('lualine').setup({
@@ -24,7 +28,7 @@ require('lualine').setup({
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'encoding', 'fileformat', 'filetype', {"fancy_macro"}},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
