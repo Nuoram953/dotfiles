b9b24a267737e0d1de35ac8322ba10c23efe6823
Nuoram <antoinemaroun@gmail.com>
2023-11-18 10:00:29 -0500

    Updated api file

--- zsh/api.zsh
+++ zsh/api.zsh
@@ -4,23 +4,27 @@ call_api(){
     local url=$1
     local data=$2
 
-    local headers="-H 'Content-Type: application/json' -H 'Accept: application/json'"
-    local user="--user '$JIRA_EMAIL:$JIRA_TOKEN'"
-    local response=$(curl -X POST $headers -d "$data" $url)
+    local user="$JIRA_EMAIL:$JIRA_TOKEN"
+    local encoded_credentials=$echo( -n $user | base64)
 
-    echo "Response: $response"
+    local response=$(curl --location $url \
+                            --header 'Content-Type: application/json' \
+                            --header "Authorization: Basic '$encoded_credentials'" \
+                            --data '{"timeSpentSeconds":'$data'}') \
+
+    echo $response | jq '.'
 }
 
 jw(){
     local number_arg=$1
     local time_arg=$2
 
-    if [[ $number_arg =~ ^[0-9]+$ ]]; then
-        echo "Number argument is: $number_arg"
-    else
-        echo "Error: The third argument must be a number."
-        return 1
-    fi
+    # if [[ $number_arg =~ ^[0-9]+$ ]]; then
+    #     echo "Number argument is: $number_arg"
+    # else
+    #     echo "Error: The third argument must be a number."
+    #     return 1
+    # fi
 
     if [[ $time_arg =~ ^([0-9]+)h?([0-9]+)m$ ]]; then
         local hours=${match[1]}
@@ -32,6 +36,6 @@ jw(){
         return 1
     fi
 
-    call_api "https://$JIRA_DOMAIN.atlassian.net/rest/api/3/issue/$number_arg/worklog" "{'timeSpentSeconds':$seconds}"
+    call_api "https://$JIRA_DOMAIN.atlassian.net/rest/api/3/issue/$number_arg/worklog" $seconds
 
 }
