0c0b748c3753e4f74ce927be5a1a7e902a1bc93e
Nuoram <antoinemaroun@gmail.com>
2023-11-18 18:51:44 -0500

    Updated python script

--- scripts/jira.py
+++ scripts/jira.py
@@ -1,15 +1,21 @@
 import json
-import base64
-import sys
 import os
+import sys
+
 import requests
+from requests.auth import HTTPBasicAuth
+
 
 def get_issues_by_boards():
     print("test")
 
+
 def get_issues_in_active_sprint(project=None):
-    url = f"https://{os.environ.get('JIRA_DOMAIN')}.atlassian.net/rest/api/3/search?maxResults=150&jql=sprint IN openSprints() AND project = {project} AND assignee= currentUser()"
-    
+    url = f"https://{os.environ.get('JIRA_DOMAIN')}.atlassian.net/rest/api/3/search?maxResults=150&jql=sprint IN openSprints() AND project = {project} AND assignee= currentUser() ORDER BY priority DESC"
+    issues = get(url)
+    issues = parse_issues(issues)
+    print_issues(issues)
+
 
 def get_issues_assigned_to_me():
     url = f"https://{os.environ.get('JIRA_DOMAIN')}.atlassian.net/rest/api/3/search?maxResults=150&jql=assignee=currentUser() AND status=Open"
@@ -20,27 +26,25 @@ def get_issues_assigned_to_me():
 def parse_issues(issues):
     output = []
     for issue in issues:
-        output.append(issue)
+        output.append({"key": issue["key"], "summary": issue["summary"]})
+        return output
+
 
 def print_issues(issues):
     for issue in issues:
-        print(issue)
+        print(issue["key"], issue["summary"])
+
 
 def get(url):
-    auth = f"{os.environ.get('JIRA_EMAIL')}:{os.environ.get('JIRA_TOKEN')}"
+    auth = HTTPBasicAuth(os.environ.get("JIRA_EMAIL"), os.environ.get("JIRA_TOKEN"))
     response = requests.get(
-            url=url,
-            headers={
-                'Authorization': f"Basic {base64.b64encode(auth)}"
-                }
-            )
+        url=url,
+        headers={"Content-Type": "application/json", "Accept": "application/json"},
+        auth=auth,
+    )
 
-    return parse_issues(json.loads(response.text)["issues"])
-
-if __name__ == '__main__':
-    try:
-        globals()[sys.argv[1]](sys.argv[2])
-    except:
-        globals()[sys.argv[1]]()
+    return json.loads(response.text)["issues"]
 
 
+if __name__ == "__main__":
+    globals()[sys.argv[1]](sys.argv[2])
