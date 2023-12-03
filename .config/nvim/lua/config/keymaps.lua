2f4aac3d848289810e57506b583a1b1b87508b8b
Nuoram <antoinemaroun@gmail.com>
2023-12-02 21:20:04 -0500

    Updated plugin and keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,50 +5,39 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
-vim.keymap.set("i", "jj", "<ESC>" ,{noremap=true})
-vim.keymap.set("t", "jj", "<C-\\><C-n>", {noremap=true})
+vim.keymap.set("i", "jj", "<ESC>", { noremap = true })
+vim.keymap.set("t", "jj", "<C-\\><C-n>", { noremap = true })
 -- Telescope
 -- <leader> is a space now
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc="Find files"})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
--- vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>so", builtin.lsp_document_symbols, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 vim.keymap.set("n", "<leader>fp", ":Telescope projects<CR>", {})
-vim.keymap.set("n", ";", "<cmd>lua require('telescope.builtin').resume(require('telescope.themes').get_ivy({}))<cr>", opts)
 vim.keymap.set(
-  "n",
-  "<space>fb",
-  ":Telescope file_browser<CR>",
-  { noremap = true }
+	"n",
+	";",
+	"<cmd>lua require('telescope.builtin').resume(require('telescope.themes').get_ivy({}))<cr>",
+	opts
 )
+vim.keymap.set("n", "<space>e", ":Telescope file_browser<CR>", { noremap = true })
 
 -- open file_browser with the path of the current buffer
-vim.keymap.set(
-  "n",
-  "<space>fb",
-  ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-  { noremap = true }
-)
-
--- NvimTree
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", {}) -- open/close
-vim.keymap.set("n", "<leader>nr", ":NvimTreeRefresh<CR>", {}) -- refresh
-vim.keymap.set("n", "<leader>nf", ":NvimTreeFindFile<CR>", {}) -- search file
-vim.keymap.set("n", "<leader>ef", ":NvimTreeFocus<CR>", {}) -- search file
+vim.keymap.set("n", "<space>e", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
 
 -- Window management
-vim.keymap.set('n',"<leader>tv","<C-w>v",{noremap=true, desc="Vertical split"})
-vim.keymap.set('n',"<leader>th","<C-w>s",{noremap=true, desc="Horizontal split"})
-vim.keymap.set('n',"<C-h>","<C-w>h",{noremap=true})
-vim.keymap.set('n',"<C-j>","<C-w>j",{noremap=true})
-vim.keymap.set('n',"<C-k>","<C-w>k",{noremap=true})
-vim.keymap.set('n',"<C-l>","<C-w>l",{noremap=true})
-vim.keymap.set('n',"<C-Up>","<cmd>resize -2<CR>",{noremap=true})
-vim.keymap.set('n',"<C-Down>","<cmd>resize +2<CR>",{noremap=true})
-vim.keymap.set('n',"<C-Left>","<cmd>vertical resize -2<CR>",{noremap=true})
-vim.keymap.set('n',"<C-Right>","<cmd>vertical resize +2<CR>",{noremap=true})
+vim.keymap.set("n", "<leader>tv", "<C-w>v", { noremap = true, desc = "Vertical split" })
+vim.keymap.set("n", "<leader>th", "<C-w>s", { noremap = true, desc = "Horizontal split" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true })
+vim.keymap.set("n", "<C-Up>", "<cmd>resize -2<CR>", { noremap = true })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize +2<CR>", { noremap = true })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<CR>", { noremap = true })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<CR>", { noremap = true })
 
 -- Use <Tab> to cycle through buffers in tab
 vim.keymap.set("n", "<Tab>", "<cmd>:bprevious<cr>")
