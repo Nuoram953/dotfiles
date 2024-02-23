e7504b335af8ca8a1a3268837ff9cad2667b3ba5
Nuoram <antoinemaroun@gmail.com>
2024-02-23 17:09:32 -0500

    Update config & Added diffview plugin

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -9,6 +9,13 @@ return {
 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
 		},
+		popupmenu = {
+			enabled = true, -- enables the Noice popupmenu UI
+			---@type 'nui'|'cmp'
+			backend = "nui", -- backend to use to show regular cmdline completions
+			-- Icons for completion item kinds (see defaults at noice.config.icons.kinds)
+			kind_icons = {}, -- set to `false` to disable icons
+		},
 	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
