76de5bd55db8dd3047b9cf022f8b65b74cb08ab6
Nuoram <antoinemaroun@gmail.com>
2024-02-25 00:11:58 -0500

    Updated harpoon

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -15,5 +15,14 @@ return {
         vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end)
+        harpoon.settings = {
+            save_on_toggle = false,
+            sync_on_ui_close = false,
+            key = function()
+                local current_file = vim.fn.expand('%:p')
+                local parent_directory = vim.fn.fnamemodify(current_file, ':h')
+                return parent_directory
+            end
+        }
     end,
 }
