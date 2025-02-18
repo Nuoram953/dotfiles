da7aa8f27bbf1a23e17962b25424a1151a681399
Nuoram953 <antoinemaroun@gmail.com>
2025-02-17 22:13:14 -0500

    Cleanup

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -13,28 +13,14 @@ return {
 			},
 			file = {
 				fzf_opts = {
-
 					["--ansi"] = true,
 				},
 			},
-			files = {
-				git_icons = false,
-				file_icons = true,
-				color_icons = true,
-			},
-			git = {
-				git_icons = false,
-				file_icons = true,
-				color_icons = true,
-			},
 			grep = {
 				rg_opts = "--hidden --line-number --multiline",
-				git_icons = false,
-				file_icons = true,
-				color_icons = true,
 			},
 			lsp = {
-				jump_to_single_result = true,
+        includeDeclaration=false
 			},
 			defaults = {
 				formatter = "path.filename_first",
--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -39,7 +39,6 @@ return {
             }
           }
         },
-        fixjson = {},
         jdtls = {},
         yamlls = {}
       },
