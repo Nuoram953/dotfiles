4041af5fad8f099a4f23a936355b15043f615f6e
Nuoram953 <antoinemaroun@gmail.com>
2025-02-22 20:51:27 -0500

    Updated snippets

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -6,8 +6,8 @@ return {
       local gen_loader = require('mini.snippets').gen_loader
       require('mini.snippets').setup({
         snippets = {
-          gen_loader.from_file('~/.config/nvim/lua/snippets/global.json'),
           gen_loader.from_lang(),
+          gen_loader.from_file('~/.config/nvim/snippets/global.json'),
         },
       })
     end,
