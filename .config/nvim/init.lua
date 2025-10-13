28ce1e8bd4ceb550b88fa3823ebaf73a88d8c084
Nuoram953 <antoinemaroun@gmail.com>
2025-10-13 12:28:38 -0400

    first step to single file confgi migration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,35 @@ require("config.init")
 require("config.keymaps")
 require("config.options")
 
-vim.wo.wrap = true
+-- -----------------------------------------------------------------------------------------------
+-- GENERAL CONFIGURATION
+-- -----------------------------------------------------------------------------------------------
+-- Search
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wildmenu = true
+
+-- Indention
+vim.opt.autoindent = true    -- auto indentation
+vim.opt.expandtab = true     -- convert tabs to spaces
+vim.opt.shiftwidth =2   -- the number of spaces inserted for each indentation
+vim.opt.smartindent = true   -- make indenting smarter
+vim.opt.softtabstop = 2 -- when hitting <BS>, pretend like a tab is removed, even if spaces
+vim.opt.tabstop = 2     -- insert 2 spaces for a tab
+vim.opt.shiftround = true    -- use multiple of shiftwidth when indenting with "<" and ">"
+
+--
+-- -----------------------------------------------------------------------------------------------
+-- PLUGINS
+-- -----------------------------------------------------------------------------------------------
+--
+--
+-- -----------------------------------------------------------------------------------------------
+-- KEYMAPS
+-- -----------------------------------------------------------------------------------------------
+--
+--
+-- -----------------------------------------------------------------------------------------------
+-- LSP
+-- -----------------------------------------------------------------------------------------------
