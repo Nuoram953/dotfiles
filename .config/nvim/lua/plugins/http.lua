4684bc4ec267d9889d29f4b3a334261ae5386007
Nuoram953 <antoinemaroun@gmail.com>
2026-05-15 21:36:05 -0400

    add kulala nvim for making http request

---
+++ .config/nvim/lua/plugins/http.lua
@@ -0,0 +1,9 @@
+vim.pack.add({
+	"https://github.com/mistweaverco/kulala.nvim",
+})
+
+require("kulala").setup({
+	global_keymaps = true,
+	global_keymaps_prefix = "<leader>R",
+	kulala_keymaps_prefix = "<>",
+})
